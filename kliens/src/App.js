baf7830e81984eafd6e3dd927259bf3dbc19d044
szbani <szabodani2001@gmail.com>
2024-04-04 01:39:45 +0200

    convert to classes

--- kliens/src/App.js
+++ kliens/src/App.js
@@ -1,184 +1,93 @@
 import './App.css';
-//const WebSocket = require('ws');
-import { Container, Grid, Typography, TextField, Button, Select, MenuItem } from '@mui/material';
-//import * as mui from '@material-ui/core';
-//import { useState } from 'react';
+import React from 'react';
+import {Container, Grid, Typography, TextField, Button, Select, MenuItem} from '@mui/material';
+// import ws from "./ws/ws";
 
-function App() {
-  //const [projects, setProjects] = useState([]);
-  const ws = wsConnect();
-  return (
-    <div className="App">
-      <head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </head>
-      <header className="App-header">
-        <h1>Redmine</h1>
-      </header>
-      <body>
-      <Container>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom
-          sx={{ mt: 2 }}
-          >
-            Elérhető projektek
-          </Typography>
+// import Test from './Test.js';
+import Projects from './templates/Projects.js';
 
-        </Grid>
-        <Grid item xs={12}>
-          {/* insert projects */}
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom>
-            Új feladat felvétele
-          </Typography>
-          <TextField
-            sx={{ mb: 2 }}
-            label="Feladat neve"
-            variant="outlined"
-            fullWidth
-            //value={newTaskName}
-            //onChange={(e) => setNewTaskName(e.target.value)}
-          />
-          <TextField
-            sx={{ mb: 2 }}
-            label="Feladat leírása"
-            variant="outlined"
-            fullWidth
-            multiline
-            rows={4}
-            //value={newTaskDescription}
-            //onChange={(e) => setNewTaskDescription(e.target.value)}
-          />
-          <Select
-            sx={{ mb: 2 }}
-            //value={selectedDeveloper}
-            //onChange={(e) => setSelectedDeveloper(e.target.value)}
-            fullWidth
-            displayEmpty
-            defaultValue=""
-          >
-            <MenuItem value="" disabled>
-              Válassz fejlesztőt
-            </MenuItem>
-           
-          </Select>
-          <Button variant="contained" color="primary">
-            Feladat hozzáadása
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom>
-            Menedzser által létrehozott feladatok
-          </Typography>
-          {/* Menedzser által létrehozott feladatok listázása */}
-        </Grid>
-      </Grid>
-    </Container>
-      </body>
-    </div>
-  );
-}
-
-function wsConnect() {
-
-  const ws = new WebSocket('ws://localhost:8080');
-  ws.onopen = async function () {
-    console.log('Connected');
-    // ws.send('Hello Server');
-    ws.send(JSON.stringify(
-        {
-            action: 'AUTH',
-            email: 'hujeno@gmail.com',
-            password: 'password'
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTaskName: '',
+            newTaskDescription: '',
+            selectedDeveloper: '',
+            isLoaded: true
         }
-    ));
-    fetchProjects();
+    }
 
-    setTimeout(() => {
-        // ws.send(JSON.stringify(
-        //     {
-        //         action: 'UPLOAD',
-        //         type: 'addTask',
-        //         name: 'task test',
-        //         project_id: '65f3b74b77df5262b3453221',
-        //         user_id: '65f3acb946a8a82e961b3fb9',
-        //         deadline: '2025-06-25',
-        //     }));
-        // ws.send(JSON.stringify(
-        //     {
-        //         _id: '65fb4f8762c10f7c88f40f9c',
-        //         action: 'UPDATE',
-        //         type: 'updateTask',
-        //         name: 'task test updatedasd',
-        //         project_id: '65f3b74b77df5262b3453221',
-        //         user_id: '65f3acb946a8a82e961b3fb9',
-        //         deadline: '2025-07-25',
-        //     }));
-        // ws.send(JSON.stringify(
-        //     {
-        //         action: 'DELETE',
-        //         type: 'deleteTask',
-        //         _id: '65fb4f8762c10f7c88f40f9c'
-        //     }));
-          // ws.send(JSON.stringify(
-          //    {
-          //        action: 'GET',
-          //        type: 'getProjects',
-          //    }));
-    //     ws.send(JSON.stringify(
-    //         {
-    //             action: 'UPLOAD',
-    //             type: 'addManager',
-    //             name: 'B.Ödön',
-    //             email: 'bodon@gmail.com',
-    //             password: 'password'
-    //         }));
-    },5000);
+    componentDidMount() {
 
-}
+    }
 
-function fetchProjects() {
-  console.log('Fetching...');
-  ws.send(JSON.stringify(
-    {
-        action: 'GET',
-        type: 'getProjects',
-    }));
-  ws.onmessage = function (e) {
-    console.log('Received data: ' + e.data);
-    
-     // const received = JSON.parse(e.data);
-     // const obj = received[0];
-      listProjects(JSON.parse(e.data));
- 
-    
-  }
-}
+    render() {
+        return (
+            <div className="App">
+                <head>
+                    <meta name="viewport" content="initial-scale=1, width=device-width"/>
+                </head>
+                <header className="App-header">
+                    <h1>Redmine</h1>
+                </header>
+                <body>
+                <Container>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant="h4" gutterBottom sx={{mt: 2}}>
+                                Elérhető projektek
+                            </Typography>
+                            {this.state.isLoaded ? <Projects></Projects> : <p>Connecting to server...</p>}
+                        </Grid>
+                        <Grid item xs={12}>
 
-function listProjects(projects) {
-  if (projects.hasOwnProperty('message')){
-    console.log('Listázás: ' + projects.message);
-  }
-  
-}
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="h4" gutterBottom>
+                                Új feladat felvétele
+                            </Typography>
+                            <TextField
+                                sx={{mb: 2}}
+                                label="Feladat neve"
+                                variant="outlined"
+                                fullWidth
+                                //value={newTaskName}
+                                //onChange={(e) => setNewTaskName(e.target.value)}
+                            />
+                            <TextField
+                                sx={{mb: 2}}
+                                label="Feladat leírása"
+                                variant="outlined"
+                                fullWidth
+                                multiline
+                                rows={4}
+                                //value={newTaskDescription}
+                                //onChange={(e) => setNewTaskDescription(e.target.value)}
+                            />
+                            <Select
+                                sx={{mb: 2}}
+                                //value={selectedDeveloper}
+                                //onChange={(e) => setSelectedDeveloper(e.target.value)}
+                                fullWidth
+                                displayEmpty
+                                defaultValue=""
+                            >
+                                <MenuItem value="" disabled>
+                                    Válassz fejlesztőt
+                                </MenuItem>
 
-ws.onmessage = function (e) {
-    console.log('Received: ' + e.data);
-    //const received = JSON.parse(e.data);
-}
-
-ws.onclose = function () {
-    console.log('Disconnected');
-}
-
-ws.onerror = function (e) {
-    console.log('Error: ' + e.data);
-}
-
-
-  return ws;
+                            </Select>
+                            <Button variant="contained" color="primary">
+                                Feladat hozzáadása
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Container>
+                </body>
+            </div>
+        )
+            ;
+    }
 }
 
 export default App;
---
+++ kliens/src/templates/Projects.js
@@ -0,0 +1,47 @@
+import React from 'react';
+// import ws from '../ws/ws';
+import {Button} from '@mui/material';
+import {DataGrid} from '@mui/x-data-grid'
+
+class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: []
+        }
+        this.getProjects = this.getProjects.bind(this);
+        // this.setProjects = this.setProjects.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <Button variant={"outlined"} onClick={this.getProjects} sx={{mb: 3}}>Refresh projects</Button>
+                {this.state.projects.length > 0 ?
+                    <DataGrid
+                        rows={this.state.projects.map((project) => project)}
+                        columns={[{field: '_id'}, {field: 'name'}]}
+                        getRowId={(row) => row._id}
+                    ></DataGrid>
+                    : <p>'No projects'</p>
+                }
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.getProjects();
+    }
+
+    getProjects() {
+        fetch('http://localhost:8080/api/projects')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({projects: data});
+            });
+
+    }
+}
+
+export default Projects;
